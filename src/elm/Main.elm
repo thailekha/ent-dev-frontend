103a640c2deb83da51ee002bdf8a88ae3c0fc1ca
Thai Le <thailekha@yahoo.com>
2018-01-23 00:08:09 +0000

    back to starter

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,154 +1,59 @@
 module Main exposing (..)
-
 import Html exposing (..)
-import Html.Events exposing (..)
 import Html.Attributes exposing (..)
-import Components.Auth0 as Auth0
-import Components.Auth0Controller as Auth0Controller
-import Components.RoomsController as RoomsController
+import Html.Events exposing ( onClick )
+
+-- component import example
+import Components.Hello exposing ( hello )
 
 
-main : Program (Maybe Auth0.LoggedInUser) Model Msg
+-- APP
+main : Program Never Int Msg
 main =
-    Html.programWithFlags
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view
-        }
+  Html.beginnerProgram { model = model, view = view, update = update }
 
 
-type alias Model =
-    { authModel : Auth0Controller.Model
-    , roomsModel : RoomsController.Model
-    }
+-- MODEL
+type alias Model = Int
+
+model : number
+model = 0
 
 
+-- UPDATE
+type Msg = NoOp | Increment
 
--- Init
-
-
-init : Maybe Auth0.LoggedInUser -> ( Model, Cmd Msg )
-init initialUser =
-    let
-        ( roomsModel, cmd ) =
-            RoomsController.init
-    in
-        ( { authModel = Auth0Controller.init initialUser
-          , roomsModel = roomsModel
-          }
-        , Cmd.map RoomsControllerMsg cmd
-        )
-
-
-
--- Messages
-
-
-type Msg
-    = Auth0ControllerMsg Auth0Controller.Msg
-    | RoomsControllerMsg RoomsController.Msg
-
-
-
--- Subscriptions
-
-
-subscriptions : a -> Sub Msg
-subscriptions a =
-    Sub.map Auth0ControllerMsg (Auth0Controller.subscriptions a)
-
-
-
--- Update
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        Auth0ControllerMsg subMsg ->
-            let
-                ( subModel, subCmd ) =
-                    Auth0Controller.update subMsg model.authModel
-            in
-                ( { model | authModel = subModel }, Cmd.map Auth0ControllerMsg subCmd )
-
-        RoomsControllerMsg subMsg ->
-            let
-                ( subModel, subCmd ) =
-                    RoomsController.update subMsg model.roomsModel
-            in
-                ( { model | roomsModel = subModel }, Cmd.map RoomsControllerMsg subCmd )
+  case msg of
+    NoOp -> model
+    Increment -> model + 1
 
 
-
--- View
-
-
+-- VIEW
+-- Html is defined as: elem [ attribs ][ children ]
+-- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-    div
-        [ class "container"
-        , style
-            [ ( "width", "90%" )
-            , ( "background-color", "#7AAAE0" )
-            , ( "font-size", "160%" )
-            ]
+  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
+    div [ class "row" ][
+      div [ class "col-xs-12" ][
+        div [ class "jumbotron" ][
+          img [ src "static/img/elm.jpg" ] []                             -- inline CSS (via var)
+          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
+          , p [] [ text ( "Elm Webpack Starter" ) ]
+          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
+            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
+            , span[][ text "FTW!" ]
+          ]
         ]
-        [ (Html.map Auth0ControllerMsg (Auth0Controller.view model.authModel))
-        , Auth0Controller.either model.authModel
-            -- logged in (implicitly has accessToken)
-            (div []
-                [ historyButton model
-                , liftRoomsControllerView (RoomsController.view model.roomsModel)
-                , submitView model
-                ]
-            )
-            -- not logged in
-            (p [] [ text "Please login to use this app" ])
-        ]
+      ]
+    ]
+  ]
 
 
-liftRoomsControllerView : Html RoomsController.Msg -> Html Msg
-liftRoomsControllerView roomsControllerHtml =
-    Html.map RoomsControllerMsg roomsControllerHtml
-
-
-submitView : Model -> Html Msg
-submitView model =
-    div
-        [ style
-            [ ( "background-color", "#7AAAE0" )
-            , ( "text-align", "center" )
-            ]
-        ]
-        [ (case Auth0Controller.tryGetAccessToken model.authModel of
-            Just accessToken ->
-                button
-                    [ style
-                        [ ( "font-size", "160%" ) ]
-                    , onClick (RoomsController.Submit accessToken)
-                    ]
-                    [ text "Submit" ]
-                    |> liftRoomsControllerView
-
-            Nothing ->
-                p [] [ text "Acess Token unavailable or expired" ]
-          )
-        ]
-
-
-historyButton : Model -> Html Msg
-historyButton model =
-    case Auth0Controller.tryGetAccessToken model.authModel of
-        Just accessToken ->
-            button
-                [ style
-                    [ ( "font-size", "110%" ) ]
-                , onClick (RoomsController.SubmitReqHistory accessToken)
-                ]
-                [ text "View history" ]
-                |> liftRoomsControllerView
-
-        Nothing ->
-            p [] [ text "Acess Token unavailable or expired" ]
+--reqAllRooms : Cmd Msg
+--reqAllRooms =
+--    Http.get ("https://rooms-checker-go.herokuapp.com/api/public/rooms") allRoomsDecoder
+--        |> RemoteData.sendRequest
+--        |> Cmd.map OnAllRoomsResponse
